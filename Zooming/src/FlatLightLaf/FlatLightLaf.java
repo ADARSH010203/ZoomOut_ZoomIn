c3b94f3a6f5a11e0777fcddd5c6f871d5d72d590
Adarsh Kumar Singh <155250714+ADARSH010203@users.noreply.github.com>
2024-09-29 14:21:10 +0530

    Add files via upload

---
+++ Zooming/src/FlatLightLaf/FlatLightLaf.java
@@ -0,0 +1,30 @@
+package FlatLightLaf;
+
+import javax.swing.*;
+
+public class FlatLightLaf extends LookAndFeel {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getID() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public boolean isNativeLookAndFeel() {
+        return false;
+    }
+
+    @Override
+    public boolean isSupportedLookAndFeel() {
+        return false;
+    }
+}
